9ee9fe137a1e4d163ce21aa2473fb15672d91928
LXtutui <113383859+LXtutui@users.noreply.github.com>
2022-12-28 15:17:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-menuListArray = ["Pizza Vegetariana", "Pizza De Frango", "Pizza Portugesa", "Pizza Quatro Queijos", "Pizza De Calabresa"];
+var menuListArray = ["Pizza Vegetariana", "Pizza De Frango", "Pizza Portugesa", "Pizza Quatro Queijos", "Pizza De Calabresa"];
 
 function getMenu(){
     var htmldata = "";  
     menuListArray.sort();
-    for(var i=0;i<menuListArray.lenght;i++){
-        htmldata=htmldata+menuListArray[i] + '<br>'
+    for(var i=0;i<menuListArray.length;i++){
+        htmldata=htmldata+menuListArray[i] + '<br>';
     }
     document.getElementById("displayMenu").innerHTML=htmldata;
 }
 function addItem(){
     var htmldata;
-    varimgtags = '<img id = "img1" src = "images/pizzaImg.png"/>'
-    var item=document.getElementById("addItem").value;
+    var imgtags = '<img id = "img1" src = "pizzaImg.png"/>';
     menuListArray.sort();
-    htmldat=""
-    for(var i=0;i<menuListArray.lenght;i++) {
-     htmldata=htmldata+menuListArray[i] + '<br>' 
+    htmldata="";
+    for(var i=0;i<menuListArray.length;i++) {
+     htmldata=htmldata+imgtags+menuListArray[i] + '<br>' ;
     }
     document.getElementById("displayAddedMenu").innerHTML=htmldata;
 }
-function addTop(){
+function addtop(){
     var item=document.getElementById("addItem").value;
     menuListArray.push(item);
     addItem();
